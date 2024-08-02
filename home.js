607f4ebd6bae088a438491301e65970ac1c61efb
Ansh <anshpathania146@gmail.com>
2024-08-02 09:19:34 +0530

    background layer for form

--- home.js
+++ home.js
@@ -1,6 +1,12 @@
 // form click 
 
 document.getElementById('button').addEventListener('click',function(){
+    document.querySelector('.register_layer').style.display="flex"
+});
+document.querySelector('.close').addEventListener('click',function(){
+    document.querySelector('.register_layer').style.display="none"
+});
+document.getElementById('button').addEventListener('click',function(){
     document.querySelector('.register').style.display="flex"
 });
 document.querySelector('.close').addEventListener('click',function(){
